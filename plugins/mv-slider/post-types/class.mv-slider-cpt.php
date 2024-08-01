87f1986bf189a48a693181ccab0090c93c07a62a
81joker <nehad.al.timimi@gmail.com>
2024-08-01 14:56:45 +0200

    Validating and sanitizing

--- plugins/mv-slider/post-types/class.mv-slider-cpt.php
+++ plugins/mv-slider/post-types/class.mv-slider-cpt.php
@@ -6,6 +6,8 @@ if ( !class_exists('MV_Slider_Post_Type') ){
         {
             add_action( 'init', array($this , 'create_post_type'));
             add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+            // To save data from metabox
+            add_action( 'save_post', array($this , 'save_post') ,$priority = 10  , $accepted_args = 2 );
         }
         public function create_post_type(){
               // My Slider Post Type
@@ -65,6 +67,28 @@ if ( !class_exists('MV_Slider_Post_Type') ){
 
         public function add_inner_meta_boxes( $post ){
             require_once(MV_SLIDER_PATH.'views/mv-slider_metabox.php');
+        }
+
+        public function save_post($post_id){
+            if (isset($_POST['action']) && $_POST['action'] == 'editpost') {
+                // get_post_meta( $post_id:integer, $key:string, $single:boolean )
+                $old_link_text = get_post_meta( $post_id, 'mv_slider_link_text', true );
+                $new_link_text = $_POST['mv_slider_link_text'];
+                $old_link_url = get_post_meta( $post_id, 'mv_slider_link_url', true );
+                $new_link_url = $_POST['mv_slider_link_url'];
+
+                // update_post_meta( $post_id:integer, $meta_key:string, $meta_value:mixed, $prev_value:mixed )
+                if (empty($new_link_text)) {
+                    update_post_meta( $post_id, 'mv_slider_link_text', 'Some some Text' );
+                } else {
+                    update_post_meta( $post_id, 'mv_slider_link_text', sanitize_text_field( $new_link_text ), $old_link_text );
+                }
+                if (empty($new_link_url)) {
+                    update_post_meta( $post_id, 'mv_slider_link_url', 'Add Your url link' );
+                } else {
+                    update_post_meta( $post_id, 'mv_slider_link_url', esc_url_raw( $new_link_url ), $old_link_url );
+                }
+            }
         }
     }
 }
