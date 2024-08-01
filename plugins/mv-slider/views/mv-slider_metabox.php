9bb52eadfba7b46b5a0e0be1057af9a3a9a4e01f
81joker <nehad.al.timimi@gmail.com>
2024-08-01 17:40:37 +0200

    23. Metabox API - Nonces and other validations

--- plugins/mv-slider/views/mv-slider_metabox.php
+++ plugins/mv-slider/views/mv-slider_metabox.php
@@ -5,6 +5,7 @@
     //var_dump( $link_text, $link_url );?>
 
 <table class="form-table mv-slider-metabox"> 
+    <input type="hidden" name="my_slider_nonce" value="<?php echo wp_create_nonce('my_slider_nonce') ?>">
     <tr>
         <th>
             <label for="mv_slider_link_text">Link Text</label>
