c78e6895cebd959791861fafd24af11a747a1689
81joker <nehad.al.timimi@gmail.com>
2024-08-05 16:30:16 +0200

    add register_scripts to view

--- plugins/mv-slider/views/mv-slider_shortcode.php
+++ plugins/mv-slider/views/mv-slider_shortcode.php
@@ -1,38 +1,44 @@
 <h3><?php echo ( ! empty ( $content ) ) ? esc_html( $content ) : esc_html( MV_Slider_Settings::$options['mv_slider_title'] ); ?></h3>
 <div class="mv-slider flexslider ">
     <ul class="slides">
-        <?php
-            $args = array(
-                'post_type'   => 'mv-slider', 
-                'post_status' => 'publish',
-                'post__in'    => $id,
-                'orderby'     => $orderby
-            );
-            $my_query = new WP_Query($args);
-            if($my_query->have_posts()) :
-            while($my_query->have_posts()): $my_query->the_post();
+    <?php 
+    
+    $args = array(
+        'post_type' => 'mv-slider',
+        'post_status'   => 'publish',
+        'post__in'  => $id,
+        'orderby' => $orderby
+    );
 
-            $button_text = get_post_meta(get_the_ID() , 'mv_slider_link_text', true);
+    $my_query = new WP_Query( $args );
 
-            $button_url = get_post_meta(get_the_ID() , 'mv_slider_link_url', true);
-        ?>
+    if( $my_query->have_posts() ):
+        while( $my_query->have_posts() ) : $my_query->the_post();
+
+        $button_text = get_post_meta( get_the_ID(), 'mv_slider_link_text', true );
+        $button_url = get_post_meta( get_the_ID(), 'mv_slider_link_url', true );
+
+    ?>
         <li>
-            <?php echo the_post_thumbnail('full', array('class' => 'img-fluid nehad')); ?>
+        <?php the_post_thumbnail( 'full', array( 'class' => 'img-fluid' ) ); ?>
             <div class="mvs-container">
                 <div class="slider-details-container">
                     <div class="wrapper">
                         <div class="slider-title">
-                            <h2>Slider TitleX</h2>
+                            <h2><?php the_title(); ?></h2>
                         </div>
                         <div class="slider-description">
-                            <div class="subtitle">Subtitle</div>
-                            <a class="link" href="<?php echo esc_attr( $button_url ); ?>"><?php echo esc_html( $button_text ); ?></a>                        </div>
+                            <div class="subtitle"><?php the_content(); ?></div>
+                            <a class="link" href="<?php echo esc_attr( $button_url ); ?>"><?php echo esc_html( $button_text ); ?></a>
+                        </div>
                     </div>
                 </div>              
             </div>
         </li>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata();?>
-        <?php endif; ?>
+        <?php
+        endwhile; 
+        wp_reset_postdata();
+    endif; 
+    ?>
     </ul>
 </div>
