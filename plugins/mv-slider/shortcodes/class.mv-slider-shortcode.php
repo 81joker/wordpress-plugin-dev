f11e876110843a431e1292719bfd6e3ff14631fb
81joker <nehad.al.timimi@gmail.com>
2024-08-05 11:20:39 +0200

    add class shortCode

---
+++ plugins/mv-slider/shortcodes/class.mv-slider-shortcode.php
@@ -0,0 +1,48 @@
+<?php 
+
+
+if (! class_exists('MV_Slider_Shortcode')) {
+    class MV_Slider_Shortcode{
+
+        public function __construct()
+        {
+            // add_shortcode( $tag:string, $callback:callable )
+            add_shortcode( 'mv-slider', 'add_shortcode' );
+        }
+
+        public function add_shortcode($atts = array() , $content = null , $tag = ''){
+
+            // This makes our attributies always recive lowercase letters
+            $atts  = array_change_key_case((array)$atts , CASE_LOWER);
+
+            // To do default value for this 
+            // or 
+            // doesn't pass any at all, we can tell the shortcode how to behave.
+            // In our case, we know that the shortcode will receive two attributes: ID, to choose which slide
+
+            // The extract function, what it does is it takes each item from this array
+            // and turns it into a variable. So, for example, the id item here would be converted to the variable
+            extract(
+                shortcode_atts( array(
+                    'id'=> '',
+                    'roderby' => 'date'
+                 ),
+                //  This is the list of attributes passed by the user in the shortcode. If the user passes any
+                //  attribute that is not on this list up here, it will be ignored, ok? And the third parameter,
+                 $atts,
+    
+                //  optional this time, is the $tag... this parameter up here, in the add_shortcode... This allows us to
+                //   create a specific filter for our shortcode
+                $tag
+                )
+            );
+
+            if (! empty($id)) {
+
+                $id = array_map('absint' , explode(',' , $id));
+            }
+            
+        }
+
+    }
+}
